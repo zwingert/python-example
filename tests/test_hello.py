0100138b71abcb6ad98ebc9a7e1c1c5495d41ba2
Zach Wingert <56572528+zwingert@users.noreply.github.com>
2023-04-30 15:36:34 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,3 +3,6 @@ import hello
 
 def test_triangle():
     assert hello.triangle(2,2,2) == 'EQUILATERAL'
+    assert hello.triangle(3,3,3) == 'EQUILATERAL'
+    assert hello.triangle(1,2,0) == 'INVALID'
+    
