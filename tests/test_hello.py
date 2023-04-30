e99e55e519915a22d192b4cbedefa5a355732265
Zach Wingert <56572528+zwingert@users.noreply.github.com>
2023-04-30 15:56:20 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,4 +4,4 @@ import random
 def test_triangle():
     assert hello.triangle(2 ,2 ,2) == 'EQUILATERAL'
     assert hello.triangle(3 ,3 ,3) == 'EQUILATERAL'
-    assert hello.triangle(1 ,2 ,random.randint(0,3)) == 'INVALID'
+    assert hello.triangle(2 ,2 ,random.randint(0,1)) == 'INVALID'
